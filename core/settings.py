51370e43e62721f7b5fa4c6b51d82fbcf3f42967
husniddin <husniddingofforov99@gmail.com>
2024-10-21 15:46:27 +0500

    test

--- core/settings.py
+++ core/settings.py
@@ -164,8 +164,7 @@ CORS_ALLOW_METHODS = (
     "PUT",
 )
 CORS_ALLOWED_ORIGINS = [
-
-    "http://localhost:8080",
+    "http://localhost:5173",
     "http://127.0.0.1:9000",
     "https://husidev007.pythonanywhere.com/",
 ]
