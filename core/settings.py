b0f390e29b9de85a02477ea4f6cd91030830bd9b
husniddin <husniddingofforov99@gmail.com>
2024-10-21 15:50:39 +0500

    test

--- core/settings.py
+++ core/settings.py
@@ -166,7 +166,7 @@ CORS_ALLOW_METHODS = (
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:9000",
-    "https://husidev007.pythonanywhere.com/",
+    # "https://husidev007.pythonanywhere.com/",
 ]
 CORS_ALLOW_HEADERS = (
     "accept",
