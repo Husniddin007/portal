1fdf1527e8a45294fec171cf81e05fdf1342acc3
husniddin <husniddingofforov99@gmail.com>
2024-10-22 14:28:46 +0500

    update

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
-
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
@@ -10,3 +11,5 @@ urlpatterns = [
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
